b58f5695cc0b177272aa4752ea8352b04a042c1a
florence1101 <674815667@qq.com>
2026-03-02 16:39:48 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    SuperBitV2.Servo2(SuperBitV2.enServo.S1, 135)
+})
+input.onButtonPressed(Button.B, function () {
+    SuperBitV2.Servo2(SuperBitV2.enServo.S1, 105)
+})
+basic.showIcon(IconNames.Giraffe)
+SuperBitV2.Servo2(SuperBitV2.enServo.S1, 105)
